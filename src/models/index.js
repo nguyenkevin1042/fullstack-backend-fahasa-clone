36619246e4182b57dd77e5433c2155650daf9684
Tien Nguyen <17130244@st.hcmuaf.edu.vn>
2023-06-21 23:06:55 +0700

    updated (21/6/2023)

--- src/models/index.js
+++ src/models/index.js
@@ -8,20 +8,39 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-const Op = Sequelize.Op;
-
-const operatorsAliases = {
-  $like: Op.$like,
-  $not: Op.not
-}
 require("dotenv").config();
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+const customizeConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  logging: false,
+  dialectOptions:
+    process.env.DB_SSL === 'true' ?
+      {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      } : {}
+  ,
+  query: {
+    "raw": true
+  },
+  timezone: "+07:00"
 }
+
+sequelize = new Sequelize(
+  process.env.DB_DATABASE_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  customizeConfig);
 
 fs
   .readdirSync(__dirname)
