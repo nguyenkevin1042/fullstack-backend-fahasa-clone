34eba4790e76b55aea32e4b5ea22bdc437266901
Tien Nguyen <17130244@st.hcmuaf.edu.vn>
2023-06-25 15:33:22 +0700

    update (25/6/2023)

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 require("dotenv").config();
 
-// let sequelize;
+let sequelize;
 // if (config.use_env_variable) {
 //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 // } else {
